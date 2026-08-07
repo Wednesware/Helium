c86793e129b88c4292d385d482b81970254157af
vesperyn <ves@wednesware.org>
2026-08-07 22:51:17 +0200

    26.19

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from helium import Project
 
 mygame: Project = Project(__file__, "mygame")
-
-mygame.script.prints.PrintInfo()
 mg = mygame.lib.ww.getClosestMatching("mg")
-mg.logging.error("This is an error message").print()
+print(mg.MESSAGE)
